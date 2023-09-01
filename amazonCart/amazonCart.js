b8134a394084074c5dd55cb147b3629e4b72c51c
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2023-09-01 20:32:14 +0530

    Each plus button corresponds to each element and productquantities is an array now.

--- amazonCart/amazonCart.js
+++ amazonCart/amazonCart.js
@@ -1,21 +1,20 @@
 //On click event I need to update the product quantity
-const plusbutton=document.getElementById('+');
-const minusbutton=document.getElementById('-');
+const plusbuttons=document.querySelectorAll('#plus');
+const minusbuttons=document.querySelectorAll('#minus');
 //this below will return a nodelist which can be accessed using forEach loop
-const productQuantity=document.querySelectorAll('#product-quantity');
+const productQuantities=document.querySelectorAll('#product-quantity');
 
-
-plusbutton.addEventListener('click',()=>{
-    productQuantity.forEach((productElement)=>{
-        let currentquantity=parseInt(productElement.innerText);
-        productElement.innerText=currentquantity+1;
-    })
+plusbuttons.forEach((plusbutton,index)=>{
+    plusbutton.addEventListener('click',()=>{
+        let currentquantity=parseInt(productQuantities[index].innerText);
+        productQuantities[index].innerText=currentquantity+1;
 })
-minusbutton.addEventListener('click',()=>{
-    productQuantity.forEach((productElement)=>{
-        let currentquantity=parseInt(productElement.innerText);
-        if(currentquantity>0){
-        productElement.innerText=currentquantity-1;
-        }
+})
+minusbuttons.forEach((minusbutton,index)=>{
+    minusbutton.addEventListener('click',()=>{
+            let currentquantity=parseInt(productQuantities[index].innerText);
+            if(currentquantity>0){
+            productQuantities[index].innerText=currentquantity-1;
+            }
     })
 })
