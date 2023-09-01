d03bf37b7ab45a9d6ad7ea58e8b0e0d99651ac43
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2023-09-01 23:52:10 +0530

    Cart notification added.

--- amazonCart/amazonCart.js
+++ amazonCart/amazonCart.js
@@ -3,18 +3,23 @@ const plusbuttons=document.querySelectorAll('#plus');
 const minusbuttons=document.querySelectorAll('#minus');
 //this below will return a nodelist which can be accessed using forEach loop
 const productQuantities=document.querySelectorAll('#product-quantity');
+const cartquantity=document.querySelector('#cart-quantity');
+
 
 plusbuttons.forEach((plusbutton,index)=>{
     plusbutton.addEventListener('click',()=>{
         let currentquantity=parseInt(productQuantities[index].innerText);
         productQuantities[index].innerText=currentquantity+1;
+        cartquantity.innerText=parseInt(cartquantity.innerText)+1;
 })
 })
+
 minusbuttons.forEach((minusbutton,index)=>{
     minusbutton.addEventListener('click',()=>{
             let currentquantity=parseInt(productQuantities[index].innerText);
             if(currentquantity>0){
             productQuantities[index].innerText=currentquantity-1;
+            cartquantity.innerText=parseInt(cartquantity.innerText)-1;
             }
     })
 })
