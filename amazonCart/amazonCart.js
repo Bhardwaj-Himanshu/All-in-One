9ca59ae2cdb8f247aca5efc77ead3d4643083d23
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2023-09-03 12:56:53 +0530

    Trying to add localstorage.

--- amazonCart/amazonCart.js
+++ amazonCart/amazonCart.js
@@ -5,12 +5,23 @@ const minusbuttons=document.querySelectorAll('#minus');
 const productQuantities=document.querySelectorAll('#product-quantity');
 const cartquantity=document.querySelector('#cart-quantity');
 
+//Local storage code to retrieve the items data stored in localstorage
+for(let i=0;i<productQuantities.length;i++){
+    const productQuantity=localStorage.getItem(`productquantity ${i+1}`);
+    const cartQuantity=localStorage.getItem(`cartquantity`)
+    if(productQuantity || cartQuantity){
+        productQuantities[i].innerText=JSON.parse(productQuantity);
+        cartquantity.innerText=JSON.parse(cartQuantity);
+    }
+}
 
 plusbuttons.forEach((plusbutton,index)=>{
     plusbutton.addEventListener('click',()=>{
         let currentquantity=parseInt(productQuantities[index].innerText);
         productQuantities[index].innerText=currentquantity+1;
         cartquantity.innerText=parseInt(cartquantity.innerText)+1;
+        localStorage.setItem(`productquantity ${index+1}`,JSON.stringify(currentquantity+1));
+        localStorage.setItem(`cartquantity`,JSON.stringify(parseInt(cartquantity.innerText)));
 })
 })
 
@@ -20,6 +31,8 @@ minusbuttons.forEach((minusbutton,index)=>{
             if(currentquantity>0){
             productQuantities[index].innerText=currentquantity-1;
             cartquantity.innerText=parseInt(cartquantity.innerText)-1;
+            localStorage.setItem(`productquantity ${index+1}`,JSON.stringify(currentquantity-1));
+            localStorage.setItem(`cartquantity`,JSON.stringify(parseInt(cartquantity.innerText)));
             }
     })
 })
