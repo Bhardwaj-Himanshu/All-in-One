06348096358867f6a208d451e44fdf44c4f83432
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2023-09-06 12:47:03 +0530

    Get and Set request retrieved.

--- amazonCart/amazonCart.js
+++ amazonCart/amazonCart.js
@@ -6,11 +6,11 @@ const productQuantities=document.querySelectorAll('#product-quantity');
 const cartquantity=document.querySelector('#cart-quantity');
 
 //Local storage code to retrieve the items data stored in localstorage
-for(let i=0;i<productQuantities.length;i++){
-    const productQuantity=localStorage.getItem(`productquantity ${i+1}`);
+for(let index=0;index<productQuantities.length;index++){
+    const productQuantity=localStorage.getItem(`productquantity ${index}`);
     const cartQuantity=localStorage.getItem(`cartquantity`)
     if(productQuantity || cartQuantity){
-        productQuantities[i].innerText=JSON.parse(productQuantity);
+        productQuantities[index].innerText=JSON.parse(productQuantity);
         cartquantity.innerText=JSON.parse(cartQuantity);
     }
 }
@@ -18,10 +18,10 @@ for(let i=0;i<productQuantities.length;i++){
 plusbuttons.forEach((plusbutton,index)=>{
     plusbutton.addEventListener('click',()=>{
         let currentquantity=parseInt(productQuantities[index].innerText);
-        productQuantities[index].innerText=currentquantity+1;
+        productQuantities[index].innerText=parseInt(currentquantity+1);
         cartquantity.innerText=parseInt(cartquantity.innerText)+1;
-        localStorage.setItem(`productquantity ${index+1}`,JSON.stringify(currentquantity+1));
-        localStorage.setItem(`cartquantity`,JSON.stringify(parseInt(cartquantity.innerText)));
+        localStorage.setItem(`productquantity ${index}`,JSON.stringify(productQuantities[index].innerText));
+        localStorage.setItem(`cartquantity`,JSON.stringify(cartquantity.innerText));
 })
 })
 
@@ -29,10 +29,10 @@ minusbuttons.forEach((minusbutton,index)=>{
     minusbutton.addEventListener('click',()=>{
             let currentquantity=parseInt(productQuantities[index].innerText);
             if(currentquantity>0){
-            productQuantities[index].innerText=currentquantity-1;
+            productQuantities[index].innerText=parseInt(currentquantity-1);
             cartquantity.innerText=parseInt(cartquantity.innerText)-1;
-            localStorage.setItem(`productquantity ${index+1}`,JSON.stringify(currentquantity-1));
-            localStorage.setItem(`cartquantity`,JSON.stringify(parseInt(cartquantity.innerText)));
+            localStorage.setItem(`productquantity ${index}`,JSON.stringify(productQuantities[index].innerText));
+            localStorage.setItem(`cartquantity`,JSON.stringify(cartquantity.innerText));
             }
     })
 })
