a872b99337d0b714fb2e3a82dc0ec075f58daab1
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2024-04-06 09:49:18 +0530

    Responsiveness added.

--- jsTekken/jsTekken.js
+++ jsTekken/jsTekken.js
@@ -1,16 +1,17 @@
 const canvas = document.querySelector('canvas');
 const phoneWarningMessage = document.getElementById('warning-message');
-console.log(phoneWarningMessage);
 const context = canvas.getContext('2d');
 
-canvas.style.backgroundColor = 'black';
-canvas.style.width = '100vw';
-canvas.style.height = '100vh';
+// -----------------------------CANVAS DIMENSIONS-------------------------------------
+// canvas.style.backgroundColor = 'black';
+// canvas.style.width = '100vw';
+// canvas.style.height = '100vh';
+// ----------------------------------------------------------------------------------
 
+// -----------------------------SCREEN ON LOAD FUNCTIONS ----------------------------
 window.addEventListener('orientationchange', () => {
   const currentOrientation = window.orientation;
   const screenPortraitWidth = window.screen.width;
-  console.log(screenPortraitWidth);
   if (
     currentOrientation === 0 ||
     currentOrientation === 180 ||
@@ -32,4 +33,34 @@ window.onload = function () {
   window.dispatchEvent(new Event('orientationchange'));
 };
 
-console.log('Hello from js world');
+// -------------------------------------------------------------------------------
+
+class Sprite {
+  //  Just in case it does not capture canvas width or height, pass canvas in constructor as well
+  constructor(position) {
+    this.position = position;
+  }
+
+  draw(color) {
+    console.log('Drawing Rectangle');
+    context.fillStyle = color;
+    context.fillRect(
+      this.position.x,
+      this.position.y,
+      canvas.width * 0.2,
+      canvas.height * 0.1
+    );
+  }
+}
+
+const player = new Sprite({
+  x: 0,
+  y: 135,
+});
+const enemy = new Sprite({
+  x: 240,
+  y: 135,
+});
+
+player.draw('red');
+enemy.draw('blue');
