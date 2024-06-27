e2913f9ff39a6e19a1213100cf322d13cee2755b
Himanshu Bhardwaj <himanshujay261201@gmail.com>
2024-06-27 22:20:51 +0530

    pacman static files evam levels added.

---
+++ jsPacman/pacman.js
@@ -0,0 +1,59 @@
+const board = document.getElementById('board');
+const levelButtons = document.querySelectorAll('.level-button');
+
+const levelOne = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0,
+  1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0,
+  0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
+
+const levelTwo = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 0,
+  1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1,
+  1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1,
+  0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
+
+const levelThree = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1,
+  0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0,
+  1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+  0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+];
+
+function levelDrawer(levelName = levelOne) {
+  board.innerHTML = '';
+  for (let i = 0; i < 100; i++) {
+    const element = document.createElement('div');
+    element.classList.add('grid');
+    element.setAttribute('id', i);
+    if (levelName[i] == 1) {
+      element.classList.add('wall');
+    }
+    board.appendChild(element);
+  }
+}
+
+levelDrawer();
+
+levelButtons.forEach((element, elementIndex) => {
+  element.addEventListener('click', () => {
+    switch (elementIndex) {
+      case 0:
+        // console.log('level 1 should be started.');
+        levelDrawer(levelOne);
+        break;
+      case 1:
+        // console.log('level 2 should be started.');
+        levelDrawer(levelTwo);
+        break;
+      case 2:
+        // console.log('level 3 should be started.');
+        levelDrawer(levelThree);
+        break;
+      default:
+        break;
+    }
+  });
+});
